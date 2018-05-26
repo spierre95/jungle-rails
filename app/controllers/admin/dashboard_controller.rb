cc97ed3309213995b72a9348cca9df8fa06c4706
spierre95 <stephen.pierre20@gmail.com>
2018-05-26 19:52:51 +0000

    fixed category error

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,10 +1,5 @@
 class Admin::DashboardController < ApplicationController
  before_action :require_authentication
-def require_authentication
-  authenticate_or_request_with_http_basic do |u,p|
-      u== ENV['USERNAME'] &&  p == ENV['PASSWORD']
-  end
-end
 
   def show
   end
