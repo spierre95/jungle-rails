30b5b8845aabaca253d36afcec123e439312afec
spierre95 <stephen.pierre20@gmail.com>
2018-05-30 01:31:31 +0000

    Feature Spec: Add to Cart

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product detail page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+        @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    visit root_path
+    click_on("Add", match: :first)
+    expect(page).to have_content 'My Cart (1)'
+    save_and_open_screenshot
+  end
+end
+
